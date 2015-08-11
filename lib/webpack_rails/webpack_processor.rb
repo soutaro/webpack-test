05976488e0dd05f993ae8590b55c61cde04b6865
Soutaro Matsumoto <matsumoto@soutaro.com>
2015-08-12 04:50:54 +0900

    WebpackProcessor

--- lib/webpack_rails/webpack_processor.rb
+++ lib/webpack_rails/webpack_processor.rb
@@ -1,7 +1,16 @@
 require 'securerandom'
+require 'open-uri'
 
 module WebpackRails
   module WebpackProcessor
+    def self.server_pid=(val)
+      @pid = val
+    end
+
+    def self.server_pid
+      @pid
+    end
+
     def self.cache_key
       "webpack" + SecureRandom.hex(3)
     end
@@ -15,13 +24,14 @@ module WebpackRails
     end
 
     def self.run_webpack(input)
-      src = Rails.root + "tmp" + SecureRandom.hex(8)
-      dest = Rails.root + "tmp" + SecureRandom.hex(8)
+      WebpackRails::WebpackProcessor::ensure_server_started()
 
-      src.write input[:data]
-      system "webpack --config #{Rails.root+"webpack.js"} -d #{src} #{dest}"
-      src.unlink
-      dest.read.tap do dest.unlink end
+      filename = Pathname(input[:filename])
+      module_name = filename.relative_path_from(Rails.root + "app/assets/javascripts")
+
+      open("http://localhost:3001/#{module_name}") do |io|
+        io.read
+      end
     end
 
     def self.needs_webpack?(input)
@@ -35,5 +45,40 @@ module WebpackRails
         end
       }
     end
+
+    def self.ensure_server_started
+      if server_pid
+        return
+      end
+
+      self.server_pid = Process.spawn("node #{Rails.root+"server.js"}");
+
+      count = 0
+      begin
+        open("http://localhost:3001/")
+      rescue Errno::ECONNREFUSED
+        count += 1
+
+        if count > 100
+          return
+        end
+
+        sleep 0.5
+        retry
+      rescue OpenURI::HTTPError
+        return
+      end
+    end
   end
 end
+
+at_exit {
+  pid = WebpackRails::WebpackProcessor.server_pid
+  if pid
+    begin
+      Process.kill "INT", pid
+      Process.waitpid pid
+    rescue Errno::ECHILD
+    end
+  end
+}
