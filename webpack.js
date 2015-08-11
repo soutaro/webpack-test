3cdfa2ab11772909d50bc9183f0e077e37c0f38f
Soutaro Matsumoto <matsumoto@soutaro.com>
2015-08-12 03:45:59 +0900

    sebpack config

---
+++ webpack.js
@@ -0,0 +1,7 @@
+module.exports = {
+  devtool: "#inline-source-map",
+  resolve: {
+    root: ['/Users/soutaro/src/webpack-test/app/assets/javascripts']
+    // moduleDirectories: ['app/assets/javascripts']
+  }
+};
